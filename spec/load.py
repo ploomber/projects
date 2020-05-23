67c7454d7c2759415b2208d0bb6565c576bf6e0c
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-05-23 19:18:14 -0400

    creating output/ in the first task

--- spec/load.py
+++ spec/load.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import pandas as pd
 import numpy as np
 
@@ -6,6 +7,7 @@ product = None
 upstream = None
 
 # +
+Path('output').mkdir(exist_ok=True)
 data = np.random.rand(1000, 10)
 df = pd.DataFrame(data)
 df.to_csv(str(product['data']), index=False)
