e6699591b9d038a6366ef1fc1e2a0892a7469adc
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-09-15 23:29:39 -0400

    fixes co2 emissions

--- contrib/co2-emissions/raw.py
+++ contrib/co2-emissions/raw.py
@@ -9,12 +9,15 @@ product = {
     'zipped': 'output/raw.zip',
     'data': 'output/raw.csv'
 }
+# -
 
-# +
 url = 'http://api.worldbank.org/v2/en/indicator/EN.ATM.CO2E.PC?downloadformat=csv'
 urllib.request.urlretrieve(url, product['zipped'])
 
 with zipfile.ZipFile(product['zipped'], 'r') as zip_ref:
-    raw_data = zip_ref.read('API_EN.ATM.CO2E.PC_DS2_en_csv_v2_1217665.csv')
+    # the filename changes when they upload a new version...
+    names = [name for name in zip_ref.namelist() if name.startswith('API')]
+    assert len(names) == 1, 'Unknown list of files, update file to unzip...'
+    raw_data = zip_ref.read(names[0])
 
 Path(product['data']).write_bytes(raw_data)
