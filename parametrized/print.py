edecdf37cd95fcab5de16d5627ae59401c791481
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-10-15 18:36:29 -0400

    fixes ci

--- parametrized/print.py
+++ parametrized/print.py
@@ -1,12 +1,7 @@
-from pathlib import Path
-
 # + tags=["parameters"]
 upstream = None
 product = None
 some_param = None
-
-# +
-Path(product).parent.mkdir(parents=True, exist_ok=True)
 
 # +
 print('some_param: ', some_param, ' type: ', type(some_param))
