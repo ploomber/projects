b730abae26b722b998cca91e36ee74cc4c926218
Eduardo Blancas Reyes <fkq8@blancas.io>
2020-11-15 12:09:56 -0500

    updates ml-intermediate example to show how to customize output notebook

--- ml-intermediate/fit.py
+++ ml-intermediate/fit.py
@@ -23,8 +23,10 @@ X_train, X_test, y_train, y_test = train_test_split(X,
                                                     test_size=0.33,
                                                     random_state=42)
 
+# + tags=["model-training"]
 clf = RandomForestClassifier(n_estimators=100)
 clf.fit(X_train, y_train)
+# -
 
 y_pred = clf.predict(X_test)
 
