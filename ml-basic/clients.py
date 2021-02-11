5c4ef048d5fac8dddb1c86134e28904220b1fcbf
Eduardo Blancas Reyes <github@blancas.io>
2021-02-11 18:14:19 -0500

    adds params names to GCloudStorageClient

--- ml-basic/clients.py
+++ ml-basic/clients.py
@@ -4,4 +4,5 @@ from ploomber.clients import GCloudStorageClient
 def get_storage_client():
     """Example client to upload artifacts to google cloud storage
     """
-    return GCloudStorageClient('ploomber-test-bucket', 'ml-basic')
+    return GCloudStorageClient(bucket_name='ploomber-test-bucket',
+                               parent='ml-basic')
