001a6bedcc12dd85282ec87692cd46d977d2a45e
Eduardo Blancas Reyes <github@blancas.io>
2021-02-17 00:44:33 -0500

    adds training-serving skew example

--- ml-online/tests/test_train.py
+++ ml-online/tests/test_train.py
@@ -1,3 +1,4 @@
+from ploomber.executors import Serial
 from ploomber.spec import DAGSpec
 
 
@@ -7,11 +8,19 @@ def test_train():
     (doesn't check if the output is correct). It passes a sample of the data
     to make it faster.
     """
-    # DAGSpec.find automatically finds your pipeline.yaml
+    # load dag
     dag = DAGSpec.find(env={
         'products': '{{root}}/testing',
         'sample': True
     }).to_dag()
 
-    # same as calling "ploomber build --force"
-    dag.build(force=True)
+    # change executor settings: you can use "pytest --pdb" to start a debugging
+    # session if the test fails. Calling dag['task'].debug() is another
+    # option
+    dag.executor = Serial(build_in_subprocess=False, catch_exceptions=False)
+
+    # a third approach for debugging is to use: import IPython; IPython.embed()
+    # to start an interactive session at this point. To do so, you must call
+    # "pytest -s"
+
+    dag.build()
