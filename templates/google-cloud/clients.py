0aed70313563ce0dd1f31e5a435741cef9d55d51
Eduardo Blancas Reyes <github@blancas.io>
2022-05-17 09:53:13 -0500

    adds storage config to google-cloud example

--- templates/google-cloud/clients.py
+++ templates/google-cloud/clients.py
@@ -1,6 +1,11 @@
 from google.cloud.bigquery.dbapi import connect
-from ploomber.clients import DBAPIClient
+from ploomber.clients import DBAPIClient, GCloudStorageClient
 
 
-def get():
+def db():
     return DBAPIClient(connect, dict())
+
+
+def storage():
+    return GCloudStorageClient(bucket_name='my-ploomber-bucket',
+                               parent='my-pipeline')
